e07b9460fc40e6a5d797819f2f867b8651634447
Bao Doan <baodoan.ic@gmail.com>
2018-10-18 17:50:22 +0700

    feat(leaflet):demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
@@ -14,6 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     LoadingBarHttpClientModule,
     LoadingBarRouterModule,
     SharedModule,
--- src/app/leaflet/leaflet/leaflet.component.ts
+++ src/app/leaflet/leaflet/leaflet.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import 'leaflet.awesome-markers/dist/leaflet.awesome-markers';
+
+const myIcon = L.icon({
+  iconUrl: 'assets/images/marker.png',
+  iconSize: [50, 50],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+  shadowUrl: 'assets/images/shadow.png',
+  shadowSize: [30, 7],
+  shadowAnchor: [10, 15]
+});
+
 @Component({
   selector: 'app-leaflet',
   templateUrl: './leaflet.component.html',
@@ -9,21 +20,75 @@ import 'leaflet.awesome-markers/dist/leaflet.awesome-markers';
 export class LeafletComponent implements OnInit {
   map: L.Map;
   marker;
+  tileLayers;
+  popup = L.popup();
   constructor() {}
   initMap() {
-    this.map = L.map('maps', { zoomDelta: 1, zoomSnap: 0.5 }).setView([10.766102, 106.747377], 16);
-
-    L.tileLayer('http://map.stis.vn/bright//{z}/{x}/{y}.png', {
-      attribution: '&copy; FPT Information Systems'
+    this.map = L.map('maps', { zoomDelta: 1, zoomSnap: 0.5 }).setView( [10.766102, 106.747377], 14);
+    this.tileLayers = L.tileLayer('http://map.stis.vn/bright/{z}/{x}/{y}.png', {
+      attribution: '&copy; FPT Information Systems',
     }).addTo(this.map);
+    this.marker = L.marker([10.766102, 106.747377], {
+    icon: myIcon,
+    draggable: true,
+    keyboard: false,
+    title: 'marker_title',
+    alt: 'marker_alt'
+  }).addTo(this.map)
+    .bindTooltip('my tooltip text')
+    .openTooltip()
+    .bindPopup('Marker_popup');
+    // Init the map
+    // Render tiles into map
 
-    // L.marker([10.766102, 106.747377]).addTo(this.map);
-    L.marker([10.766102, 106.747377]).addTo(this.map);
-  //   L.polygon([
-  //     [51.509, -0.08],
-  //     [51.503, -0.06],
-  //     [51.51, -0.047]
-  // ]).addTo(this.map);
+    // Add marker, tooltip, popup
+    // The invoked function is assigned to this.marker
+    // so that this.marker can be used to attach an event later
+    // Add polygon, popup
+    L.polygon([
+      [10.767638, 106.746909],
+      [10.777638, 106.756909],
+      [10.787638, 106.786909]
+    ])
+      .addTo(this.map)
+      .bindPopup('Polygon_popup');
+    // Add circle, popup
+    L.circle([10.767638, 106.746909], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 260,
+    })
+      .addTo(this.map)
+      .bindPopup('Circle_popup');
+    // Standalone popup
+    this.popup
+      .setLatLng([10.767638, 106.736909])
+      .setContent('Standalone popup')
+      .openOn(this.map);
+    // Attach an event on marker
+    // this.marker.dragging.addHooks(this.getSth());
+    this.map.on('click', this.log);
+    this.map.on('click', this.movePopup);
+  }
+  movePopup(event) {
+    console.log(this.popup);
+    this.popup.setLatLng(event.latlng)
+    .setContent('clicked on ' + event.latlng).openOn(this.map);
+  }
+  log(event) {
+      console.log('Lat', event.latlng.lat, 'Lng', event.latlng.lng);
+    }
+  eventHandler() {
+    console.log('marker is moved');
+    // console.log(this.marker.getPopup());
+  }
+  getSth(): any {
+    // console.log(this.marker.getLatLng());
+    return this.marker.getLatLng();
+  }
+  setSth(lat, lng): void {
+    this.marker.setLatLng([lat, lng]);
   }
   ngOnInit() {
     this.initMap();
